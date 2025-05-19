1ab0ea07a5e1e2f7993fbbb7047df68d974b0a78
Tu Nombre <tu@email.com>
2025-05-19 12:18:32 -0300

    Netlify: run quarto render in install_quarto.sh for correct PATH

--- install_quarto.sh
+++ install_quarto.sh
@@ -2,4 +2,5 @@
 set -e
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
 tar -xzf quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
-export PATH=$PWD/quarto-${QUARTO_VERSION}/bin:$PATH 
+export PATH=$PWD/quarto-${QUARTO_VERSION}/bin:$PATH
+quarto render 
